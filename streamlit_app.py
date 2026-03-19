6b3785a98b7f9414586f0dbfb4b946fcdf6e8b70
GeorgeNagamura <ssamuca@gmail.com>
2026-03-19 18:57:04 -0300

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
+import xlrd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+workbook = xlrd.open_workbook("rep-C4A20WV8-210803.xls", ignore_workbook_corruption=True)
+df = pd.read_excel(workbook)
+
+
+
+st.title("🎈 Casamento Rafa e Giu 🎈 🎉")
+st.dataframe(data=df)
